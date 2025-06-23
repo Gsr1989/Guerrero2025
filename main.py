8a019d92422dae66c1cfba80df1e5e960c1d65b6
Gsr1989 <empresalorens@gmail.com>
2025-06-23 17:29:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -172,5 +172,15 @@ def logout():
 def registro_admin():
     return redirect(url_for('registro_usuario'))
 
+@app.route('/registros_admin')
+def registros_admin():
+    if 'admin' not in session:
+        return redirect(url_for('login'))
+
+    response = supabase.table("folios_registrados").select("*").order("fecha_expedicion", desc=True).execute()
+    registros = response.data if response.data else []
+
+    return render_template("registros_admin.html", registros=registros)
+
 if __name__ == '__main__':
     app.run(debug=True)
