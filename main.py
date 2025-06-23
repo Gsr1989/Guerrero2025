830930d3dcb9d4f8f1ca9d33418fe03c5970759c
Gsr1989 <empresalorens@gmail.com>
2025-06-23 16:35:20 -0600

    Update and rename main (1).py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory
 from datetime import datetime, timedelta
 from supabase import create_client, Client
