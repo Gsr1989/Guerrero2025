f7f1c26010286bb9f71c99f56c7d79ae3a2ad773
Gsr1989 <empresalorens@gmail.com>
2025-06-23 20:24:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -195,6 +195,14 @@ def logout():
 
 from flask import send_from_directory
 
+@app.route('/ver_registros')
+def ver_registros():
+    if 'admin' not in session:
+        return redirect(url_for('login'))
+    response = supabase.table("folios_registrados").select("*").order("fecha_expedicion", desc=True).execute()
+    registros = response.data if response.data else []
+    return render_template("mis_registros.html", registros=registros)
+
 @app.route('/descargar_pdf/<folio>')
 def descargar_pdf(folio):
     ruta_archivo = f"static/pdfs/{folio}.pdf"
